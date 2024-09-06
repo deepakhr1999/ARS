2f4572722acbef1b5c68a54015e69d914a098249
deepakhr1999 <deepakhr1999@gmail.com>
2024-09-06 20:07:07 +0530

    hparams tuning experiment

--- code/main_grid.py
+++ code/main_grid.py
@@ -3,7 +3,7 @@ import json
 import time
 from ars import run_ars
 import ray
-
+from sklearn.model_selection import ParameterGrid
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Run an ARS experiment.")
@@ -24,7 +24,7 @@ def parse_arguments():
 
 
 def load_config(env_name):
-    config_path = f"configs/paper/{env_name}.json"
+    config_path = f"configs/grids/{env_name}.json"
     with open(config_path, encoding="utf-8") as file:
         return json.load(file)
 
@@ -33,24 +33,27 @@ def main():
     args = parse_arguments()
     config = load_config(args.env_name)
 
-    experiment_params = {
-        "env_name": args.env_name,
-        "n_iter": 1000,
+    experiment_params_grid = {
+        "env_name": [args.env_name],
+        "n_iter": [1000],
         "n_directions": config["N"],
         "deltas_used": config["b"],
         "step_size": config["alpha"],
         "delta_std": config["nu"],
-        "n_workers": 10,
-        "rollout_length": 1000,
-        "shift": config["shift"],
-        "seed": 237,
-        "policy_type": "linear",
-        "dir_path": "data",
-        "filter": "MeanStdFilter",
+        "n_workers": [10],
+        "rollout_length": [1000],
+        "shift": [config["shift"]],
+        "seed": [0],
+        "policy_type": ["linear"],
+        "dir_path": ["data"],
+        "filter": ["MeanStdFilter"],
     }
 
-    for seed in range(4):
-        experiment_params["seed"] = seed
+    for experiment_params in ParameterGrid(experiment_params_grid):
+        if experiment_params["n_directions"] < experiment_params["deltas_used"]:
+            continue
+        print("Running for params")
+        print(json.dumps(experiment_params, indent=4))
         experiment_params["dir_path"] = f"data/{args.env_name}/{time.time()}"
         run_ars(experiment_params)
 
